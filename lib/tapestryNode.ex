4e66b233d2e4625e9e394f3e14012404cd96b2c2
subhambgh <subhama.sit@gmail.com>
2019-10-19 22:57:48 -0400

    routing table

--- lib/tapestryNode.ex
+++ lib/tapestryNode.ex
@@ -3,16 +3,44 @@ defmodule TapestryNode do
 
     def start_link(x,_,numRequests) do
         input_srt = Integer.to_string(x)
-        nodeid = Base.encode16(:crypto.hash(:sha, input_srt))
+        nodeid = String.slice(Base.encode16(:crypto.hash(:sha, input_srt)),0..7)
         GenServer.start_link(__MODULE__, {nodeid,numRequests}, name: String.to_atom("n#{nodeid}"))
     end
 
     def init({selfid,numRequests}) do
-        routetable = Matrix.from_list([[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[],[]])
+        routetable = Matrix.from_list([
+          [],[],[],[],[],[],[],[]
+          ])
         {:ok, {selfid,routetable,numRequests,0}}
     end
 
-    def handle_cast({:intialize_routing_table},{selfid,routetable,req,num_created})do
-      
+    def match(nodeid,selfid) do
+      index = Enum.find_index(0..7, fn i -> String.at(nodeid,i) != String.at(selfid,i) end)
+      #IO.puts "index = #{index} #{nodeid} #{selfid}"
+      case index do
+        0 ->
+          {0,elem(Integer.parse(String.at(nodeid,0),16),0)}
+        _->
+          {index,elem(Integer.parse(String.at(nodeid,index),16),0)}
+      end
+    end
+
+    def routeTableBuilder(routetable, [],selfid) do
+      routetable
+    end
+
+    def routeTableBuilder(routetable, [head | tail],selfid) do
+      {level,slot} = match(head,selfid)
+      newroute = put_in routetable[level][slot], head
+      routeTableBuilder(newroute, tail,selfid)
+    end
+
+    def handle_cast({:intialize_routing_table,num_created},{selfid,routetable,numRequests,0})do
+        hashList = Enum.map 1..num_created, fn(x) -> String.slice(Base.encode16(:crypto.hash(:sha, Integer.to_string(x))),0..7) end
+        hashList = hashList -- [selfid]
+        routetable = routeTableBuilder( routetable, hashList,selfid )
+        #Matrix.to_list(routetable)
+        IO.puts("#{selfid} #{inspect routetable}")
+        {:noreply, {selfid,routetable,numRequests,0}}
     end
 end
